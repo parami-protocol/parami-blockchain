f42ab9b9b8c44d8030d099f721647337bd7a8f61
ruibinx <xingrobin@gmail.com>
2022-06-17 19:12:54 +0800

    fixup! mod: add more migration tests

--- pallets/ad/src/migrations.rs
+++ pallets/ad/src/migrations.rs
@@ -2,9 +2,11 @@ use crate::StorageVersion;
 use crate::{Config, Pallet};
 use frame_support::generate_storage_alias;
 use frame_support::migration::*;
-use frame_support::traits::OnRuntimeUpgradeHelpersExt;
 use frame_support::{pallet_prelude::*, traits::Get, weights::Weight};
 use sp_runtime::traits::Saturating;
+
+#[cfg(feature = "try-runtime")]
+use frame_support::traits::OnRuntimeUpgradeHelpersExt;
 
 pub fn migrate<T: Config>() -> Weight {
     let version = StorageVersion::get::<Pallet<T>>();
